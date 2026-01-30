217430fdb397d2c2cc86e2a971c62bdc4ea586b6
humarco008 <hu.marco@iisgalvanimi.edu.it>
2026-01-30 09:42:59 +0000

    punto 4 ora 2

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 let voto = 0; 
 function piu() {
-   document.getElementById("voto").innerHTML = voto;
+  
   if (voto == 10) {
     alert("Limite raggiunto");
   } else {
     voto++;
+     document.getElementById("voto").innerHTML = voto;
   }
  if (voto >= 6) {
     document.getElementById("voto").style.color = "green";
   } else {
     document.getElementById("voto").style.color = "red";
   }
+    document.getElementById("stelle").innerHTML = "";
+ for (let i = 1; i <= voto; i++) {
+    document.getElementById("stelle").innerHTML += "🌟";
+  }
+   if (voto == 10) {
+    document.getElementById("stelle").innerHTML += " <b>TOP!</b>";
+  }
 }
 function meno() {
-   document.getElementById("voto").innerHTML = voto;
+  
   if (voto == 0) {
     alert("Limite raggiunto");
   } else {
     voto--;
-  
+     document.getElementById("voto").innerHTML = voto;
   }
  if (voto >= 6) {
     document.getElementById("voto").style.color = "green";
   } else {
     document.getElementById("voto").style.color = "red";
   }
-
-}
   document.getElementById("stelle").innerHTML = "";
  for (let i = 1; i <= voto; i++) {
     document.getElementById("stelle").innerHTML += "🌟";
   }
+   if (voto == 10) {
+    document.getElementById("stelle").innerHTML += " <b>TOP!</b>";
+  }
+}
 
